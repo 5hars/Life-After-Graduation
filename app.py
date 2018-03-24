3cf295d2045d531a11e7575da5e72ebbbf1b92c8
Satya Madakshira <34543758+satyafly@users.noreply.github.com>
2018-03-24 13:38:10 -0400

    Add files via upload
    Superscrapers - uploading the latest versions of the files

---
+++ app.py
@@ -0,0 +1,44 @@
+# import necessary libraries
+import json
+from flask import (Flask, render_template, jsonify, request)
+import pandas as pd
+import numpy as np
+import sqlalchemy 
+from sqlalchemy.ext.automap import automap_base 
+from sqlalchemy.orm import Session 
+from sqlalchemy import create_engine, desc 
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__, template_folder='templates')
+
+# Create a list to hold our data
+# engine= create_engine("sqlite:///heatmap.sqlite")
+# Base = automap_base()
+# Base.prepare(engine, reflect = True )
+
+# Cities = Base.classes.cities
+# Houses = Base.classes.houses
+# Jobs = Base.classes.jobs
+# Recreation = Base.classes.recreation
+
+# session = Session(engine)
+
+@app.route("/")
+def homepage():
+	return render_template("index.html")
+
+# @app.route('/names')
+
+# def names():
+# 	results = session.query(Cities).statement
+# 	df = pd.read_sql_query(results, session.bind)
+# 	df.set_index("id", inplace = True)
+
+# 	return jsonify(list(df.columns))
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
